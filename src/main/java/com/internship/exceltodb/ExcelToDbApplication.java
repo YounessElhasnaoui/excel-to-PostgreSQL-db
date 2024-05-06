ed26f7cd12bc8981f91362097178556a181dc1a1
YounessElhasnaoui <youness.elhasnaoui1@usmba.ac.ma>
2024-05-06 21:40:47 +0100

    update main method (add libraries and EnableJpaRepositories,EntityScan )

--- src/main/java/com/internship/exceltodb/ExcelToDbApplication.java
+++ src/main/java/com/internship/exceltodb/ExcelToDbApplication.java
@@ -1,14 +1,18 @@
 package com.internship.exceltodb;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.internship.exceltodb.repository")
+@EntityScan(basePackages = "com.internship.exceltodb.model")
 public class ExcelToDbApplication implements CommandLineRunner {
 
 	@Autowired
