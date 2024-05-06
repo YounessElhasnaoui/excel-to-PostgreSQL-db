f177ebb5a2f0acc4ec55a7e76a2f4d6a13fae8f8
YounessElhasnaoui <youness.elhasnaoui1@usmba.ac.ma>
2024-05-06 21:36:55 +0100

    update user class

--- src/main/java/com/internship/exceltodb/model/User.java
+++ src/main/java/com/internship/exceltodb/model/User.java
@@ -1,14 +1,12 @@
 package com.internship.exceltodb.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private int userId;
 
@@ -36,43 +34,98 @@ public class User {
     @Column(name = "user_city")
     private String city;
 
-    @Column(name = "user_event")
-    private int eventId;
-
     @Column(name = "user_role")
     private String role;
 
+    @Column(name = "user_event")
+    private int eventId;
+
     // Getters and Setters
-    public int getUserId() { return userId; }
-    public void setUserId(int userId) { this.userId = userId; }
+    public int getUserId() {
+        return userId;
+    }
 
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getFirstName() {
+        return firstName;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-    public int getDobYear() { return dobYear; }
-    public void setDobYear(int dobYear) { this.dobYear = dobYear; }
+    public String getLastName() {
+        return lastName;
+    }
 
-    public int getDobMonth() { return dobMonth; }
-    public void setDobMonth(int dobMonth) { this.dobMonth = dobMonth; }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-    public int getDobDay() { return dobDay; }
-    public void setDobDay(int dobDay) { this.dobDay = dobDay; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getGender() { return gender; }
-    public void setGender(String gender) { this.gender = gender; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
+    public int getDobYear() {
+        return dobYear;
+    }
 
-    public int getEventId() { return eventId; }
-    public void setEventId(int eventId) { this.eventId = eventId; }
+    public void setDobYear(int dobYear) {
+        this.dobYear = dobYear;
+    }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public int getDobMonth() {
+        return dobMonth;
+    }
+
+    public void setDobMonth(int dobMonth) {
+        this.dobMonth = dobMonth;
+    }
+
+    public int getDobDay() {
+        return dobDay;
+    }
+
+    public void setDobDay(int dobDay) {
+        this.dobDay = dobDay;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
 }
